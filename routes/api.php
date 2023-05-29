67bd7a28180e44895224dd6db5db586042718bf9
LeoSallu <L.sallustio99@gmail.com>
2023-05-29 14:34:20 +0200

    feat: api route for types

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\ProjectController;
+use App\Http\Controllers\Api\TypeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -22,4 +23,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('projects',[ProjectController::class,'index']);
 
 Route::get('projects/{id}',[ProjectController::class,'show']);
+
+Route::get('types',[ApiTypeController::class,'index']);
+
+Route::get('types/{id}',[TypeController::class,'show']);
+
+
 
